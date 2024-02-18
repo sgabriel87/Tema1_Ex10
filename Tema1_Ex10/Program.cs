1a7455e8c2413a2728efaaf9929039eb7b828881
Gabriel <Gabriel@GABRIEL>
2024-02-18 20:06:20 +0200

    Add project files.

---
+++ Tema1_Ex10/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la
+tastatura.
+Numarul are exact 2 cifre*/
+
+Console.WriteLine("Introduceti un numar întreg cu exact doua cifre:");
+int numar = Convert.ToInt32(Console.ReadLine());
+
+// Extragem prima cifră prin impartire
+int cifra1 = numar / 10;
+
+// Extragem a doua cifra prin calculul restului
+int cifra2 = numar % 10;
+
+// Calculam suma
+int sumaCifrelor = cifra1 + cifra2;
+
+Console.WriteLine("Suma cifrelor numarului {0} este {1}", numar, sumaCifrelor);
+
